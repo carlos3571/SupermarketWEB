d00b9aad07e886dc2e3b3153b7f03a5e492e4ea8
carlos3571 <carlosandresorozcoillera@gmail.com>
2024-11-02 01:15:26 -0500

    _Layout providers y paymodel

--- obj/Debug/net8.0/SupermarketWEB.AssemblyInfo.cs
+++ obj/Debug/net8.0/SupermarketWEB.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SupermarketWEB")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16d7bfd212e37b4191293913f856d4680fbbc7b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2498ea673462f4ca4f0261b4bcf7e477b8eaafa4")]
 [assembly: System.Reflection.AssemblyProductAttribute("SupermarketWEB")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SupermarketWEB")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
