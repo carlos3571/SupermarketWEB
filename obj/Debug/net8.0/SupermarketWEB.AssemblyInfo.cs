2498ea673462f4ca4f0261b4bcf7e477b8eaafa4
carlos3571 <carlosandresorozcoillera@gmail.com>
2024-11-02 01:09:56 -0500

    Create  Paymode y Provedirs

--- obj/Debug/net8.0/SupermarketWEB.AssemblyInfo.cs
+++ obj/Debug/net8.0/SupermarketWEB.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SupermarketWEB")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ddf0c5eeeed86c24158d5eb3359c3958f3800b9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16d7bfd212e37b4191293913f856d4680fbbc7b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("SupermarketWEB")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SupermarketWEB")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
